cfb278b58195f3f9377ba59c481bf69395c9bd08
Vivek Chandra Sardar <chandravivek52@gmail.com>
2023-12-29 16:00:57 +0530

    initial-commit ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+
+  name : string | undefined;
+
+  onNameChange(event: any) {
+    this.name = event.target.value;
+  }
 }
